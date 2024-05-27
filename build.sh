37a7defb2cc652abdeb90845289f9f66fed17004
leepand <leepand@leepanddeMacBook-Pro.local>
2024-05-27 08:45:34 +0800

    update

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Uninstall the Python package using pip
+yes | pip uninstall tinynet
+
+# Read the version from pyproject.toml and store it in a variable
+version=$(awk -F'"' '/version =/ {print $2}' pyproject.toml)
+
+# Print the version
+echo "Version: $version"
+
+if [ -d "dist" ]; then
+  rm -rf dist
+fi
+
+if [ -d "robingrad.egg-info" ]; then
+  rm -rf robingrad.egg-info
+fi
+
+# build the package
+python -m build
+
+# install builded package 
+cd dist
+command="pip install tinynet-${version}.tar.gz"
+eval $command
+
+
---
+++ tinynet/nn/__init__.py
@@ -0,0 +1,31 @@
+from ..tensor import Tensor
+from .loss import MSELoss, BCELoss
+
+
+class Linear:
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bias: bool = True,
+        mean: float = 0.0,
+        std: float = 1.0,
+        int_type: str = "he",
+    ):
+        if int_type == "xavier":
+            self.weight = Tensor.xavier(in_features, out_features, requires_grad=True)
+        elif int_type == "he":
+            self.weight = Tensor.he(in_features, out_features, requires_grad=True)
+        else:
+            self.weight = Tensor.normal(
+                mean, std, (out_features, in_features), requires_grad=True
+            )
+
+        self.bias = (
+            Tensor.normal(mean, 0, (1, out_features), requires_grad=True)
+            if bias
+            else None
+        )
+
+    def __call__(self, x: "Tensor") -> "Tensor":
+        return x.linear(self.weight.T, self.bias)
